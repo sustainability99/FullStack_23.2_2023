8301bb687c38a3a81db213885f57a37452706282
sustainability99 <126771099+sustainability99@users.noreply.github.com>
2023-03-19 19:32:42 +0100

    update

--- Lesson_42_16.03/code/money/src/Main.java
+++ Lesson_42_16.03/code/money/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         Money euro = new Money(200, "EUR");
         Money euro2 = new Money(200, "eur");
-        Money dollar = new Money(300,"USD");
+        Money dollar = new Money(200,"USD");
 
         System.out.println(euro);
         System.out.println(euro2);
@@ -13,10 +13,12 @@ public class Main {
 
         System.out.println(euro == euro2);
         System.out.println(euro == dollar);
+        System.out.println(euro2 == dollar);
         System.out.println();
 
         System.out.println(euro.equals(euro2));
         System.out.println(euro.equals(dollar));
+        System.out.println(euro2.equals(dollar));
 
         System.out.println(euro2.toString());;
 
